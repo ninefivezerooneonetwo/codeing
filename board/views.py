165a854fa3c03f5b95afe8ca131f8b086e444f29
lks1212 <ahdghks1212@gmail.com>
2022-03-07 16:24:41 +0900

    파일 경로 변경 및 수정

--- board/views.py
+++ board/views.py
@@ -16,58 +16,58 @@ class MyView(View):
         };
         return render(request,'home.html',context);
 
-    @request_mapping("/notice", method="get") #공지사항
+    @request_mapping("/notice/notice", method="get") #공지사항
     def notice(self, request):
         objs = Board.objects.all();
         context = {
             'objs': objs
         };
-        return render(request, 'notice.html', context);
+        return render(request, 'notice/notice.html', context);
 
-    @request_mapping("/info", method="get") #정보게시판
+    @request_mapping("/info/info", method="get") #정보게시판
     def info(self, request):
         objs = Board.objects.all();
         context = {
             'objs': objs
         };
-        return render(request, 'info.html', context);
+        return render(request, 'info/info.html', context);
 
-    @request_mapping("/free", method="get") #자유게시판
+    @request_mapping("/free/free", method="get") #자유게시판
     def free(self, request):
         objs = Board.objects.all();
         context = {
             'objs':objs
         };
-        return render(request, 'free.html',context);
+        return render(request, 'free/free.html',context);
 
-    @request_mapping("/qna", method="get") #질문게시판
+    @request_mapping("/qna/qna", method="get") #질문게시판
     def qna(self, request):
         objs = Board.objects.all();
         context = {
             'objs': objs
         };
-        return render(request, 'qna.html',context);
+        return render(request, 'qna/qna.html',context);
 
-    @request_mapping("/project", method="get") #프로젝트
+    @request_mapping("/project/project", method="get") #프로젝트
     def project(self, request):
         objs = Board.objects.all();
         context = {
             'objs': objs
         };
-        return render(request, 'project.html', context);
+        return render(request, 'project/project.html', context);
 
-    @request_mapping("/study", method="get") #스터디
+    @request_mapping("/study/study", method="get") #스터디
     def study(self, request):
         objs = Board.objects.all();
         context = {
             'objs': objs
         };
-        return render(request, 'study.html',context);
+        return render(request, 'study/study.html',context);
 
     @request_mapping("/post", method="get")
     def post(self, request):
         return render(request, 'post.html');
 
-    @request_mapping("/codeingwiki", method="get")
+    @request_mapping("/wiki/wiki", method="get")
     def wiki(self, request):
-        return render(request, 'wiki.html');
+        return render(request, 'wiki/wiki.html');
