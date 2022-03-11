a6530dcd75f02eba5e4b8d3c207a5da894422ff4
pyw4609 <wook4609@naver.com>
2022-03-12 00:56:12 +0900

    스크랩 기능 / 상세 페이지 기능 구현
    # clip에 clip.html, detail.html, postok.html 추가
    # 각 게시판 html에 스크랩 기능 추가
    # base.html 스크랩 부분 수정

--- board/views.py
+++ board/views.py
@@ -9,9 +9,84 @@ from django.utils import timezone
 @request_mapping("")
 class MyView(View):
 
+    @request_mapping("/notice/scrap/", method="get")
+    def notice_scrap(self, request):
+        """
+        스크랩 함수
+        :param request:
+        :return:
+        """
+
+        from .models import Clipping
+        from django.shortcuts import get_object_or_404
+
+        if 'sessionid' in request.session :
+            type = request.GET['type']
+            board = Board.objects.get(board_id=request.GET['board_id'])
+            user = User.objects.get(user_id=request.session['sessionid'])
+            try :
+                cliped = Clipping.objects.get(user=user
+                                              , board=board)  # 스크랩여부 체크
+            except :
+                cliped = None
+            if not cliped :
+                clip = Clipping(user = user,
+                                board = board)
+                clip.save()
+                suc = 'Y'
+            else :
+                suc = 'dup'
+            url = '/' + type + '/' + type
+        else :
+            url = '/login'
+            suc = 'login'
+
+
+        context = {'url' : url,
+                   'suc' : suc}
+        return render(request, 'clip/postok.html', context)
+
+    @request_mapping("/scrap/scrap/", method="get")
+    def scrap(self, request):
+        """
+        스크랩 리스트 화면
+        """
+        from django.shortcuts import redirect
+        from .models import Clipping
+        # 로그인 체크
+        if 'sessionid' in request.session:
+            user = User.objects.get(user_id=request.session['sessionid'])
+            clips = Clipping.objects.filter(user_id=user) # 로그인한 유저가 등록한 board_id 가져오기
+            boards = []
+            if clips:
+                for clip in clips :
+                    boards.append(Board.objects.get(board_id = clip.board_id)) # board_id를 이용하여 Board정보 가져오기
+
+            page = request.GET.get('page', '1')
+            paginator = Paginator(boards, '10')
+            page_obj = paginator.get_page(page)
+            context = {'objs':page_obj}
+
+            return render(request, 'clip/clip.html', context)
+        else :
+            return redirect('/login')
+
+    @request_mapping("/clip/detail/", method="get")
+    def scrap_detail(self, request):
+        from django.shortcuts import redirect
+        if 'sessionid' in request.session:
+            board = Board.objects.get(board_id=request.GET['board_id']) # board_id로 Board정보 가져오기
+            context = {'board':board}
+
+            return render(request, 'clip/detail.html', context)
+        else :
+            return redirect('/login')
+    # ================================================================
+
     @request_mapping("/", method="get")
     def home(self,request):
         objs = Board.objects.order_by('-board_date').all();
+
         context = {
             'objs': objs
         };
